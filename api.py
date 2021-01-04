4f8def0baffbf3dc3d5d4953716a6f6d33cedf7c
Suhas <suhasr@berkeley.edu>
2021-01-04 01:26:49 -0500

    optional stats

--- api.py
+++ api.py
@@ -1,81 +1,160 @@
 import requests
 import json
-from constants import team_dict 
-from classes import Team, Stats, Player
-from typing import Tuple, Dict
+from constants import team_dict
+from classes import Team, Stats, Player, Schedule, Matchup, Week, UserMatchup
+from typing import Dict, Any, Optional, cast
+from functools import cached_property
+
 
 class ESPN_API:
     fantasy_filter = {
-            "players":
-                {
-                "filterSlotIds":{"value":[0,1,2,3,4,5,6,7,8,9,10,11]},
-                "filterRanksForScoringPeriodIds":{"value":[12]},
-                "limit":1200,
-                "sortPercOwned":{"sortAsc":"false","sortPriority":1},
-                "sortDraftRanks":{"sortPriority":100,"sortAsc":"true","value":"STANDARD"},
-                "filterRanksForRankTypes":{"value":["STANDARD"]},
-                "filterStatsForTopScoringPeriodIds":{"value":5,"additionalValue":["002021","102021","002020","012021","022021","032021","042021"]}
-                }
+        "players": {
+            "filterSlotIds": {"value": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]},
+            "filterRanksForScoringPeriodIds": {"value": [12]},
+            "limit": 1200,
+            "sortPercOwned": {"sortAsc": "false", "sortPriority": 1},
+            "sortDraftRanks": {
+                "sortPriority": 100,
+                "sortAsc": "true",
+                "value": "STANDARD",
+            },
+            "filterRanksForRankTypes": {"value": ["STANDARD"]},
+            "filterStatsForTopScoringPeriodIds": {
+                "value": 5,
+                "additionalValue": [
+                    "002021",
+                    "102021",
+                    "002020",
+                    "012021",
+                    "022021",
+                    "032021",
+                    "042021",
+                ],
+            },
         }
+    }
 
-    def __init__(self):
-        with open('config.json') as config_file:
+    def __init__(self) -> None:
+        with open("config.json") as config_file:
             config = json.load(config_file)
-        self.url = "http://fantasy.espn.com/apis/v3/games/fba/seasons/2021/segments/0/leagues/" + str(config["league_id"])
+        self.url = (
+            "http://fantasy.espn.com/apis/v3/games/fba/seasons/2021/segments/0/leagues/"
+            + str(config["league_id"])
+        )
         self.cookies = config["cookies"]
-    
-    def get_req(self, params=None, headers=None):
+        # Find a way to determine this, probably input the user's name
+        #  and cross against teams view
+        self.user_tid = 9
+
+    def get_req(
+        self,
+        params: Optional[Dict[str, str]] = None,
+        headers: Optional[Dict[str, str]] = None,
+    ) -> Any:
         return requests.get(
-            self.url,
-            cookies=self.cookies,
-            params=params,
-            headers=headers
+            self.url, cookies=self.cookies, params=params, headers=headers
         ).json()
 
-    def get_teams(self):
-        team_json = self.get_req(params={"view":"mTeam"})
+    def get_players_json(self) -> Dict[Any, Any]:
+        player_json = self.get_req(
+            params={"view": "kona_player_info"},
+            headers={
+                "x-fantasy-filter": json.dumps(self.fantasy_filter),
+                "x-fantasy-platform": "kona-PROD-a5abcf16cafe5c335041277beeafcabc2a236402",
+                "x-fantasy-source": "kona",
+            },
+        )
+        return cast(Dict[Any, Any], player_json)
+
+    @property
+    def teams(self) -> Dict[int, Team]:
+        team_json = self.get_req(params={"view": "mTeam"})
         teams = {}
-        for team in team_json['teams']:
-            teams[team['id']] = Team(
-                tid = team['id'],
-                location = team['location'],
-                nickname = team['nickname'],
-                abbr = team['abbrev'],
-                waiver_rank = team['waiverRank']
+        for team in team_json["teams"]:
+            teams[team["id"]] = Team(
+                league_tid=team["id"],
+                location=team["location"],
+                nickname=team["nickname"],
+                abbr=team["abbrev"],
+                waiver_rank=team["waiverRank"],
             )
         return teams
 
-    def get_player_info(self) -> Tuple[Dict[int, Player], Dict[str, int]]:
-        player_json = self.get_req(
-            params = {"view": 'kona_player_info'},
-            headers={
-                'x-fantasy-filter': json.dumps(self.fantasy_filter),
-                'x-fantasy-platform': "kona-PROD-a5abcf16cafe5c335041277beeafcabc2a236402",
-                'x-fantasy-source': 'kona'
-            }
-        )
+    @cached_property
+    def name_to_pid(self) -> Dict[str, int]:
+        player_json = self.get_players_json()
+        return {
+            player["player"]["fullName"]: player["id"]
+            for player in player_json["players"]
+        }
+
+    @property
+    def players(self) -> Dict[int, Player]:
+        player_json = self.get_players_json()
         players = {}
-        name_to_pid = {}
-        for player in player_json['players']:
-            player_obj = player['player']
-            players[player['id']] = Player(
-                pid = player['id'],
-                name = player_obj['fullName'],
-                status = player['status'],
-                stats = Stats.parse_stats(player_obj['stats']),
-                league_tid = player['onTeamId'],
-                nba_tid = player_obj['proTeamId'],
+        for player in player_json["players"]:
+            player_obj = player["player"]
+            players[player["id"]] = Player(
+                pid=player["id"],
+                name=player_obj["fullName"],
+                status=player["status"],
+                stats=Stats.parse_stats(player_obj["stats"]),
+                league_tid=player["onTeamId"],
+                nba_tid=player_obj["proTeamId"],
             )
-            name_to_pid[player_obj['fullName']] = player['id']
-        return players, name_to_pid
+        return players
 
+    @property
+    def schedule(self):
+        matchup_json = self.get_req(params={"view": "mMatchup"})
+        schedule = Schedule()
+        for week in range(16):
+            matchups = {}
+            for i in range(6):
+                curr_matchup = matchup_json["schedule"][week * 6 + i]
 
+                matchups[curr_matchup["id"]] = Matchup(
+                    matchup_id=curr_matchup["id"],
+                    period_id=curr_matchup["matchupPeriodId"],
+                    home_tid=curr_matchup["home"]["teamId"],
+                    away_tid=curr_matchup["away"]["teamId"],
+                    home_stat=Schedule.get_stats_from_matchup(curr_matchup["home"]),
+                    away_stat=Schedule.get_stats_from_matchup(curr_matchup["away"]),
+                    winner=None
+                    if curr_matchup["winner"] == "UNDECIDED"
+                    else curr_matchup["winner"],
+                )
+            if curr_matchup["winner"] != "UNDECIDED":
+                schedule.last_processed_period = curr_matchup[
+                    "matchupPeriodId"
+                ]  # Update last processed period id
+            schedule.weeks.append(Week(week_id=week, matchups=matchups))
+        return schedule
 
+    @property
+    def matchup(self):
+        schedule = self.schedule
+        curr_period_id = schedule.last_processed_period + 1
+        curr_week = schedule.weeks[curr_period_id - 1]
+        for matchup in curr_week.matchups.values():
+            if matchup.home_tid == self.user_tid:
+                user_stat = matchup.home_stat
+                opp_stat = matchup.away_stat
+                opp_tid = matchup.away_tid
+            elif matchup.away_tid == self.user_tid:
+                user_stat = matchup.away_stat
+                opp_stat = matchup.home_stat
+                opp_tid = matchup.home_tid
 
+        assert user_stat and opp_stat
 
-
-
-
+        return UserMatchup(
+            matchup_id=matchup.matchup_id,
+            period_id=curr_period_id,
+            opp_tid=opp_tid,
+            user_stat=user_stat,
+            opp_stat=opp_stat,
+        )
 
     # @staticmethod
     # def load_team_dict():
--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
-from typing import Optional, Set, Dict
-from dataclasses import dataclass
+from typing import Optional, Dict, List, Any, Callable, cast, Union
+from dataclasses import dataclass, field
+
 
 @dataclass
 class Stat:
@@ -11,44 +12,68 @@ class Stat:
     ftm: float
     fta: float
     made_threes: float
-    attempted_threes: float
-    off_rebounds: float
-    def_rebounds: float
     rebounds: float
     assists: float
     steals: float
     blocks: float
-    fouls: float
+    # attempted_threes: float
+    # off_rebounds: float
+    # def_rebounds: float
+    # fouls: float
+
     @property
     def fg_pct(self) -> float:
-        return self.fgm/self.fga
+        return self.fgm / self.fga
 
     @property
     def ft_pct(self) -> float:
-        return self.ftm/self.fta
-    
+        return self.ftm / self.fta
+
     @staticmethod
-    def parse_stat(stat_obj):
-        def from_stat(index):
-            return stat_obj[index] if index in stat_obj else None
+    def from_stat_basic(stat_obj: Dict[int, float]) -> Callable[[int], Optional[float]]:
+        # Player info returns a dict of pure stats
+        return lambda index: stat_obj[index] if index in stat_obj else None
+
+    @staticmethod
+    def from_stat_matchup(
+        stat_obj: Dict[int, Dict[str, Any]]
+    ) -> Callable[[int], Optional[float]]:
+        # Matchup info returns a dict where raw stat is listed under score
+        return (
+            lambda index: cast(float, stat_obj[index]["score"])
+            if index in stat_obj
+            else None
+        )
+
+    @staticmethod
+    def parse_stat(
+        stat_obj: Union[Dict[int, Dict[str, Any]], Dict[int, float]],
+        from_stat_type: str = "basic",
+    ) -> "Stat":
+        if from_stat_type == "basic":
+            from_stat = Stat.from_stat_basic(cast(Dict[int, float], stat_obj))
+        else:
+            from_stat = Stat.from_stat_matchup(
+                cast(Dict[int, Dict[str, Any]], stat_obj)
+            )
 
         return Stat(
-            points = from_stat('0'),
-            blocks = from_stat('1'),
-            steals = from_stat('2'),
-            assists = from_stat('3'),
-            off_rebounds = from_stat('4'),
-            def_rebounds = from_stat('5'),
-            rebounds = from_stat('6'),
-            fouls = from_stat('9'),
-            turnovers = from_stat('11'),
-            fgm = from_stat('13'),
-            fga = from_stat('14'),
-            ftm = from_stat('15'),
-            fta = from_stat('16'),
-            made_threes = from_stat('17'),
-            attempted_threes = from_stat('18'),
-            minutes = from_stat('40')
+            points=from_stat("0"),
+            blocks=from_stat("1"),
+            steals=from_stat("2"),
+            assists=from_stat("3"),
+            rebounds=from_stat("6"),
+            turnovers=from_stat("11"),
+            fgm=from_stat("13"),
+            fga=from_stat("14"),
+            ftm=from_stat("15"),
+            fta=from_stat("16"),
+            made_threes=from_stat("17"),
+            minutes=from_stat("40"),
+            # attempted_threes = from_stat('18'),
+            # fouls = from_stat('9'),
+            # off_rebounds = from_stat('4'),
+            # def_rebounds = from_stat('5'),
         )
 
         # 7,8 = ?
@@ -56,51 +81,58 @@ class Stat:
         # ft_pct = 20
         # 3_pct = 21
         # efg = 22
-    
+
+
 @dataclass
 class StatDict:
     avg: Stat
     total: Stat
-        
+
+
 @dataclass
 class Stats:
-    last7: StatDict
-    last15: StatDict
-    last30: StatDict
-    s20: StatDict
-    s21: StatDict
-    s21_proj: StatDict
+    last7: Optional[StatDict]
+    last15: Optional[StatDict]
+    last30: Optional[StatDict]
+    s20: Optional[StatDict]
+    s21: Optional[StatDict]
+    s21_proj: Optional[StatDict]
 
     @staticmethod
-    def parse_stats(stats):
-        def get_stat(stat_id):
-            stat_objs = [stat for stat in stats if stat['id'] == stat_id]
+    def parse_stats(stats: Dict[Any, Any]) -> "Stats":
+        def get_stat(stat_id: str) -> Optional[StatDict]:
+            stat_objs = [stat for stat in stats if stat["id"] == stat_id]
             if not stat_objs:
                 return None
             stat_obj = stat_objs[0]
             return StatDict(
-                avg = Stat.parse_stat(stat_obj['averageStats']) if 'averageStats' in stat_obj else None, 
-                total= Stat.parse_stat(stat_obj['stats']))
+                avg=Stat.parse_stat(stat_obj["averageStats"])
+                if "averageStats" in stat_obj
+                else None,
+                total=Stat.parse_stat(stat_obj["stats"]),
+            )
 
         return Stats(
-            last7 = get_stat('012021'),
-            last15 = get_stat('022021'),
-            last30 = get_stat('032021'),
-            s20 = get_stat('002020'),
-            s21 = get_stat('002021'),
-            s21_proj = get_stat('102021')
+            last7=get_stat("012021"),
+            last15=get_stat("022021"),
+            last30=get_stat("032021"),
+            s20=get_stat("002020"),
+            s21=get_stat("002021"),
+            s21_proj=get_stat("102021"),
         )
+
 
 @dataclass
 class Player:
     pid: int
     name: str
-    status: str # FREEAGENT, WAIVERS, etc.
+    status: str  # FREEAGENT, WAIVERS, etc.
     stats: Stats
     league_tid: int
     nba_tid: int
 
     games_remaining: Optional[int] = None
+
 
 @dataclass
 class Team:
@@ -113,6 +145,47 @@ class Team:
     roster: Optional[Dict[int, Player]] = None
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.location + " " + self.nickname
 
+
+@dataclass
+class Matchup:
+    matchup_id: int
+    period_id: int
+    home_tid: int
+    away_tid: int
+    home_stat: Stat
+    away_stat: Stat
+
+    winner: Optional[int] = None  # None if currently active
+
+
+@dataclass
+class Week:
+    week_id: int
+    matchups: Dict[int, Matchup]
+
+
+@dataclass
+class Schedule:
+    weeks: List[Week] = field(default_factory=list)  # 16 weeks
+    # Store last processed period to avoid repeat processing (1-indexed)
+    last_processed_period: int = 0
+
+    @staticmethod
+    def get_stats_from_matchup(curr_matchup_side) -> Optional[Stat]:
+        if "cumulativeScore" in curr_matchup_side:
+            return Stat.parse_stat(
+                curr_matchup_side["cumulativeScore"]["scoreByStat"], "matchup"
+            )
+        return None
+
+
+@dataclass
+class UserMatchup:
+    matchup_id: int
+    period_id: int
+    opp_tid: int
+    user_stat: Stat
+    opp_stat: Stat
